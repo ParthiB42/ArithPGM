d4a135a54ae0c9fca4be5c273451f10c77f8f096
ParthiB42 <132385801+ParthiB42@users.noreply.github.com>
2023-05-02 23:10:14 +0530

    Changed WS-C to WS-D

--- ArithPGM.cbl
+++ ArithPGM.cbl
@@ -11,14 +11,14 @@
        WORKING-STORAGE SECTION.
        01 WS-A PIC 9(2).
        01 WS-B PIC 9(2).
-       01 WS-C PIC 9(2).
+       01 WS-D PIC 9(2).
        PROCEDURE DIVISION.
        ADD-PARA.
            DISPLAY "PLEASE ENTER THE A VALUE"
            ACCEPT WS-A.
            DISPLAY "PLEASE ENTER THE B VALUE"
            ACCEPT WS-B.
-           ADD WS-A , WS-B GIVING WS-C.
-           DISPLAY "THE TOTAL OF A + B IS :" WS-C.
+           ADD WS-A , WS-B GIVING WS-D.
+           DISPLAY "THE TOTAL OF A + B IS :" WS-D.
            STOP RUN.
        END PROGRAM ArithPGM.
