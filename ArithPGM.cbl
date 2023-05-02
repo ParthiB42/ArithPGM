a6ad4a40d004d0df069348ed3a79d3f5320e0b4b
ParthiB42 <parthibanspb42@gmail.com>
2023-05-03 00:35:16 +0530

    FULL CALC

--- ArithPGM.cbl
+++ ArithPGM.cbl
@@ -5,14 +5,25 @@
        WORKING-STORAGE SECTION.
        01 WS-A PIC 9(2).
        01 WS-B PIC 9(2).
-       01 WS-D PIC 9(2).
+       01 WS-C PIC 9(2).
+       01 WS-OPE PIC X(1).
        PROCEDURE DIVISION.
        ADD-PARA.
            DISPLAY "PLEASE ENTER THE A VALUE"
            ACCEPT WS-A.
            DISPLAY "PLEASE ENTER THE B VALUE"
            ACCEPT WS-B.
-           SUBTRACT WS-A FROM WS-B GIVING WS-D.
-           DISPLAY "THE TOTAL OF A + B IS :" WS-D.
+           DISPLAY "PLEASE ENTER THE OPERATION"
+           ACCEPT WS-OPE.
+           IF WS-OPE = '+' THEN
+           ADD WS-A, WS-B GIVING WS-C.
+
+           IF WS-OPE = '-' THEN
+           SUBTRACT WS-A FROM WS-B GIVING WS-C.
+
+           IF WS-OPE = '*' THEN
+               MULTIPLY WS-A BY WS-B GIVING WS-C.
+
+           DISPLAY "THE TOTAL OF A + B IS :" WS-C.
            STOP RUN.
        END PROGRAM ArithPGM.
